b9f7904723963bb38941274ad371a787ef2fadbe
Felix <felixcm1129@hotmail.ca>
2020-12-08 17:36:01 -0500

    the end?

--- app/controllers/admin/recette_controller.rb
+++ app/controllers/admin/recette_controller.rb
@@ -1,5 +1,6 @@
 class Admin::RecetteController < AdminController
-    
+    before_action :get_recettes_id 
+
     def recette
         respond_to do |format|
             format.html{render 'admin/recette'}
